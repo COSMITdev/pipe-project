00e9563183d56100be2b214e236151caabcac891
Patrick Müller <patrick.e.muller@gmail.com>
2016-06-01 11:34:55 -0300

    Adds Users CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+if Rails.env.development?
+  10.times do |i|
+    User.create!(email: "user_#{i+1}@example.com",
+                 name: "User Name #{i+1}",
+                 password: "123123123")
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
+  factory :user do
+
+  end
+
   factory :admin_user do
-    
+
   end
 end
