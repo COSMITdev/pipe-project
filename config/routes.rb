044aac4afde6488d338abbaaae6bc49c42a0a18a
Gabriel Carpenedo <gabriel.carpenedo@gmail.com>
2016-06-02 16:30:43 -0400

    Changes file names and routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   get 'projects_index',		to: 'pages#projects_index',		as: :projects_index
+  get 'dashboard_index',		to: 'pages#dashboard_index',		as: :dashboard_index
   get 'threads_index', 		to: 'pages#threads_index', 		as: :threads_index
   get 'thread_show', 		to: 'pages#thread_show', 		as: :thread_show
   get 'tasks_index', 		to: 'pages#tasks_index', 		as: :tasks_index
