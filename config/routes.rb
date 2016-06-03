a30d14e3e9f0fd6950182c1cf57f647223f2c22b
Patrick Müller <patrick.e.muller@gmail.com>
2016-06-03 14:51:17 -0300

    Fixes typo for routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'pages#home'
+  root to: 'pages#home'
 
   resources :projects, except: :destroy do
     resources :topics, except: [:destroy] do
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     post 'send_invitation', to: 'projects#send_invitation', as: :send_invitation
   end
 
-  get 'tasks_index',      to: 'pages#tasks_index',      as: :tasks_index
+  get 'tasks_index', to: 'pages#tasks_index', as: :tasks_index
 end
